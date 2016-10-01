6d65abe2009dd42ba2388c208f51d66ca150d9f4
Guo Yunhe <guoyunhebrave@gmail.com>
2016-10-01 16:57:27 +0300

    Clean Plasma cache

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QProcess>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +12,20 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_plasmaCacheButton_clicked()
+{
+    QProcess process;
+    process.start("kquitapp", QStringList() << "plasmashell");
+    process.waitForFinished();
+    QStringList rm_args;
+    rm_args << "-R";
+    rm_args << "~/.cache/*.kcache";
+    rm_args << "~/.cache/plasma*";
+    process.start("rm", rm_args );
+    process.waitForFinished();
+    process.start("kstart", QStringList() << "plasmashell");
+    process.waitForFinished();
+    return;
 }
